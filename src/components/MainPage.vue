262df0ac9b78b0bc27c3a2355215dabbf45a3742
Vladimir Abramtsov <abramtsov.vn@gmail.com>
2024-06-03 14:09:12 +0300

    html

---
+++ src/components/MainPage.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from "vue";
+import ky from "ky";
+
+defineProps({
+  msg: String,
+});
+
+const json = await ky
+  .get(
+    "https://api.openweathermap.org/data/2.5/weather?q=moscow&appid=d59df76d4159ffd1335453f3f101f469"
+  )
+  .json();
+
+console.log(json);
+</script>
+
+<template>
+  <form action="">
+    <input type="text" placeholder="Enter Location" />
+    <input type="submit" value="&#128269;" />
+  </form>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
