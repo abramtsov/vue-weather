9a449c287464462a0c6958ffaa83a878fde4ba46
Vladimir Abramtsov <abramtsov.vn@gmail.com>
2024-11-05 17:23:46 +0300

    steel working

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -161,8 +161,6 @@ onBeforeMount(showFav);
             width="2rem"
             version="1.1"
             id="Layer_1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
             viewBox="0 0 512 512"
             xml:space="preserve"
           >
@@ -273,11 +271,8 @@ onBeforeMount(showFav);
 </template>
 
 <style scoped>
-@import url("https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&family=Montserrat+Alternates:wght@500;600&family=Raleway:wght@600&display=swap");
 .read-the-docs {
   color: #888;
 }
-/* .animate__animated {
-  animation-duration: 1.2s;
-} */
 </style>
